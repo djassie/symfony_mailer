0943c629a4a4f93b2bd3f2a3c115546c73479b2c
AdamPS <AdamPS@2650563.no-reply.drupal.org>
2021-08-17 17:57:12 +0100

    Improved version

--- src/Email.php
+++ src/Email.php
@@ -2,61 +2,206 @@
 
 namespace Drupal\symfony_mailer;
 
+use Drupal\Component\Render\MarkupInterface;
+use Drupal\Component\Render\PlainTextOutput;
+use Symfony\Component\Mailer\Transport\TransportInterface;
 use Symfony\Component\Mime\Email as SymfonyEmail;
 
 class Email extends SymfonyEmail {
 
   protected $key;
-  protected $content;
+  protected $content = [];
   protected $isHtml = TRUE;
   protected $libraries = [];
+
+  /**
+   * The mail transport.
+   *
+   * @var Symfony\Component\Mailer\Transport\TransportInterface
+   */
+  protected $transport;
+
+  protected $data = [];
   protected $tokenReplace = FALSE;
   protected $tokenData;
   protected $tokenOptions;
+  protected $sending = FALSE;
 
+  /**
+   * Constructs the Email object.
+   *
+   * @param string $key
+   *   Message key, in the form "MODULE.TYPE".
+   */
   public function __construct($key) {
     parent::__construct();
     $this->key = $key;
   }
 
+  /**
+   * Gets the message key.
+   *
+   * @return string
+   *   Message key, in the form "MODULE.TYPE".
+   */
   public function getKey() {
     return $this->key;
   }
 
-  public function setContent($content) {
+  /**
+   * Sets the content to use for creating the HTML/plain email body.
+   *
+   * Use this function instead of calling text() or html() directly.
+   *
+   * @param array $content
+   *   Render array for the email body content. This will be rendered using a
+   *   template that can add header or footer markup.
+   *
+   * @return $this
+   */
+  public function content(array $content) {
     $this->content = $content;
     return $this;
+  }
+
+  /**
+   * Appends content to use for creating the HTML/plain email body.
+   *
+   * @param array $content
+   *   Array to append to the content render array.
+   *
+   * @return $this
+   */
+  public function appendContent(array $content) {
+    $name = 'n' . count($this->content);
+    $this->content[$name] = $content;
+    return $this;
+  }
+
+  /**
+   * Appends string content to use for creating the HTML/plain email body.
+   *
+   * @param string $text
+   *   String to append to the content render array in a paragraph tag.
+   *
+   * @return $this
+   */
+  public function appendParagraph(string $text) {
+    $element = [
+      '#markup' => $text,
+      '#prefix' => '<p>',
+      '#suffix' => '</p>',
+    ];
+    return $this->appendContent($element);
   }
 
   public function getContent() {
     return $this->content;
   }
 
-  public function setHtml($is_html) {
+  /**
+   * Sets whether to send the email as HTML.
+   *
+   * @param bool $is_html
+   *   TRUE to send as HTML content type, FALSE to send as plain text.
+   *
+   * @return $this
+   */
+  public function enableHtml($is_html) {
     $this->isHtml = $is_html;
     return $this;
   }
 
+  /**
+   * Queries whether to send the email as HTML.
+   *
+   * @return bool
+   *   TRUE to send as HTML content type, FALSE to send as plain text.
+   */
   public function isHtml() {
     return $this->isHtml;
   }
 
+  /**
+   * Adds an asset library to use as mail CSS.
+   *
+   * @param string $library
+   *   Library name, in the form "THEME/LIBRARY".
+   *
+   * @return $this
+   */
+  public function addLibrary($library) {
+    $this->libraries[] = $library;
+    return $this;
+  }
+
+  /**
+   * Gets the libraries to use as mail CSS.
+   *
+   * @return array
+   *   Array of library names, in the form "THEME/LIBRARY".
+   */
   public function getLibraries() {
     return $this->libraries;
   }
 
   /**
-   * Enables tokens replacement in the message subject and body.
+   * Sets the mail transport to use.
+   *
+   * @param Symfony\Component\Mailer\Transport\TransportInterface $transport
+   *   Transport interface.
+   *
+   * @return $this
+   */
+  public function setTransport(TransportInterface $transport) {
+    $this->transport = $transport;
+    return $this;
+  }
+
+  /**
+   * Gets the mail transport to use.
+   *
+   * @return Symfony\Component\Mailer\Transport\TransportInterface
+   *   Transport interface.
+   */
+  public function getTransport() {
+    return $this->transport;
+  }
+
+  /**
+   * Sets data to pass to the email template and use in token replacement.
    *
    * @param array $data
    *   (optional) An array of keyed objects.
+   *
+   * @return $this
+   */
+  public function data(array $data = []) {
+    $this->data = $data;
+    return $this;
+  }
+
+  /**
+   * Gets data to pass to the email template and use in token replacement.
+   *
+   * @return array
+   *   An array of keyed objects.
+   */
+  public function getData() {
+    return $this->data;
+  }
+
+  /**
+   * Enables tokens replacement in the message subject and body.
+   *
    * @param array $options
    *   (optional) A keyed array of settings and flags to control the token
    *   replacement process.
+   *
+   * @return $this
    */
-  public function enableTokenReplace(array $data = [], array $options = []) {
+  public function enableTokenReplace(array $options = []) {
     $this->tokenReplace = TRUE;
-    $this->tokenData = $data;
     $this->tokenOptions = $options;
     return $this;
   }
@@ -65,12 +210,49 @@ class Email extends SymfonyEmail {
     return $this->tokenReplace;
   }
 
-  public function getTokenData() {
-    return $this->tokenData;
-  }
-
   public function getTokenOptions() {
     return $this->tokenOptions;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function subject($subject) {
+    // @todo Is this safe in the global render context? Could instead save the
+    // unaltered subject and render it in sending().
+    if ($subject instanceof MarkupInterface) {
+      $subject = PlainTextOutput::renderFromHtml($subject);
+    }
+    return parent::subject($subject);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function text($body, string $charset = 'utf-8') {
+    if (!$this->sending) {
+      throw new \exception('Use the content() method to set the message body.');
+    }
+    return parent::text($body, $charset);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function html($body, string $charset = 'utf-8') {
+    if (!$this->sending) {
+      throw new \exception('Use the content() method to set the message body.');
+    }
+    return parent::html($body, $charset);
+  }
+
+  /**
+   * Marks that the email is sending.
+   *
+   * @internal
+   */
+  public function sending() {
+    $this->sending = TRUE;
   }
 
 }
--- src/Mailer.php
+++ src/Mailer.php
@@ -2,9 +2,8 @@
 
 namespace Drupal\symfony_mailer;
 
-use Drupal\Component\Render\PlainTextOutput;
+use Drupal\Component\Render\MarkupInterface;
 use Drupal\Component\Utility\Html;
-use Drupal\Core\Asset\AssetCollectionRendererInterface;
 use Drupal\Core\Asset\AssetResolverInterface;
 use Drupal\Core\Asset\AttachedAssets;
 use Drupal\Core\Config\ConfigFactoryInterface;
@@ -14,11 +13,9 @@ use Drupal\Core\Render\RenderContext;
 use Drupal\Core\Render\RendererInterface;
 use Drupal\Core\Utility\Token;
 use Html2Text\Html2Text;
+use Symfony\Component\Mailer\Exception\RuntimeException;
 use Symfony\Component\Mailer\Mailer as SymfonyMailer;
-use Symfony\Component\Mailer\Transport\Dsn;
-use Symfony\Component\Mailer\Transport\NativeTransportFactory;
-use Symfony\Component\Mailer\Transport\SendmailTransportFactory;
-use Symfony\Component\Mailer\Transport\Smtp\EsmtpTransportFactory;
+use Symfony\Component\Mailer\Transport;
 use Symfony\Component\Mime\Address;
 use Symfony\Component\Mime\Header\UnstructuredHeader;
 use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
@@ -64,25 +61,13 @@ class Mailer implements MailerInterface {
   protected $assetResolver;
 
   /**
-   * The CSS asset collection renderer service.
-   *
-   * @var \Drupal\Core\Asset\AssetCollectionRendererInterface
-   */
-  protected $cssCollectionRenderer;
-
-  /**
    * The configuration factory.
    *
    * @var \Drupal\Core\Config\ConfigFactoryInterface
    */
   protected $configFactory;
 
-  /**
-   * The Symfony Mailer.
-   *
-   * @var \Symfony\Component\Mailer\MailerInterface
-   */
-  protected $mailer;
+  protected $defaultTransport;
 
   /**
    * Constructs the Mailer object.
@@ -102,16 +87,16 @@ class Mailer implements MailerInterface {
    * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
    *   The configuration factory.
    */
-  public function __construct(EventDispatcherInterface $dispatcher, RendererInterface $renderer, ModuleHandlerInterface $module_handler, Token $token, AssetResolverInterface $asset_resolver, AssetCollectionRendererInterface $css_collection_renderer, ConfigFactoryInterface $config_factory) {
+  public function __construct(EventDispatcherInterface $dispatcher, RendererInterface $renderer, ModuleHandlerInterface $module_handler, Token $token, AssetResolverInterface $asset_resolver, ConfigFactoryInterface $config_factory) {
     $this->dispatcher = $dispatcher;
     $this->renderer = $renderer;
     $this->moduleHandler = $module_handler;
     $this->token = $token;
     $this->assetResolver = $asset_resolver;
-    $this->cssCollectionRenderer = $css_collection_renderer;
     $this->configFactory = $config_factory;
-    $this->createTransports();
-    $this->mailer = new SymfonyMailer($this->transports['native'], NULL, $this->dispatcher);
+    // @todo Maybe should override the options to pass -bs.
+    // @see https://swiftmailer.symfony.com/docs/sending.html
+    $this->defaultTransport = Transport::fromDsn('native://default');
   }
 
   /**
@@ -132,11 +117,6 @@ class Mailer implements MailerInterface {
    * @internal
    */
   public function doSend(Email $message) {
-    // Call hooks
-    // @todo Also hook_email_KEY_alter().
-    $this->moduleHandler->alter('email', $message);
-    $message->getHeaders()->addTextHeader('X-Transport', 'smtp');
-
     // Set defaults.
     $site_config = $this->configFactory->get('system.site');
     $site_mail = $site_config->get('mail') ?: ini_get('sendmail_from');
@@ -153,45 +133,37 @@ class Mailer implements MailerInterface {
     }
     $message->getHeaders()->addTextHeader('X-Mailer', 'Drupal');
 
-    // Render body.
-    $content = $message->getContent();
-    if (is_array($content)) {
-      $content = $this->renderer->renderPlain($content);
+    if (!$message->getLibraries()) {
+      // @todo Configure mail theme.
+      $mail_theme = \Drupal::theme()->getActiveTheme()->getName();
+      $message->addLibrary("$mail_theme/email");
+    }
+    if (!$message->getTransport()) {
+      $message->setTransport($this->defaultTransport);
     }
 
-    // Replace tokens.
-    $subject = $message->getSubject('subject');
-    if ($message->requiresTokenReplace()) {
-      if (!($subject instanceof MarkupInterface)) {
-        $subject = Html::escape($subject);
-      }
-      $subject = $this->token->replace($subject, $message->getTokenData(), $message->getTokenOptions());
-      $content = $this->token->replace($content, $message->getTokenData(), $message->getTokenOptions());
-    }
-
-    // Set subject.
-    if ($subject instanceof MarkupInterface) {
-      $subject = PlainTextOutput::renderFromHtml($subject);
-    }
-    $message->subject($subject);
+    // Call hooks
+    // @todo Also hook_email_KEY_alter().
+    $this->moduleHandler->alter('email', $message);
 
     // Set body.
     $is_html = $message->isHtml();
-    $this->setBody($message, $content, $is_html);
-    if ($is_html && empty($message->getTextBody())) {
+    $this->setBody($message, $is_html);
+    // @todo Allow caller to supply a custom plain text alternative.
+    if ($is_html) {
       // Set plain text alternative.
-      $this->setBody($message, $content, FALSE);
+      $this->setBody($message, FALSE);
     }
 
     // Send.
-    $this->mailer->send($message);
-  }
-
-  protected function createTransports() {
-    // @todo Read config and write to framework:mailer:transports:xxx
-    // @see https://symfony.com/doc/current/mailer.html#multiple-email-transports
-    $this->transports['native'] = (new NativeTransportFactory())->create(new Dsn('native', 'default'));
-    $this->transports['sendmail'] = (new SendmailTransportFactory())->create(new Dsn('sendmail', 'default'));
+    $mailer = new SymfonyMailer($message->getTransport(), NULL, $this->dispatcher);
+    try {
+      ksm($message);
+      $mailer->send($message);
+    }
+    catch (RuntimeException $e) {
+      \Drupal::messenger()->addWarning($e->getMessage());
+    }
   }
 
   /**
@@ -204,31 +176,9 @@ class Mailer implements MailerInterface {
    *
    * @internal
    */
-  protected function setBody(Email $message, $content, $is_html) {
-    // @todo Read configured $text_format =
-    // $text_format = $this->configFactory->get('symfony_mailer.settings')->get('text_format') ?: NULL;
-    $text_format = NULL;
-
-    if (!($content instanceof MarkupInterface)) {
-      if ($is_html) {
-        // Convert to HTML. The default 'plain_text' format escapes markup,
-        // converts new lines to <br> and converts URLs to links.
-        $content = check_markup($content, $text_format);
-      }
-      else {
-        // The body will be plain text. However we need to convert to HTML
-        // to render the template then convert back again. Use a fixed
-        // conversion because we don't want to convert URLs to links.
-        $content = preg_replace("|\n|", "<br />\n", HTML::escape($content)) . "<br />\n";
-      }
-    }
-
-    // Convert relative URLs to absolute.
-    $content = Markup::create(Html::transformRootRelativeUrlsToAbsolute((string) $content, \Drupal::request()->getSchemeAndHttpHost()));
-
+  protected function setBody(Email $message, $is_html) {
     $render = [
       '#theme' => 'email',
-      '#content' => $content,
       '#message' => $message,
       '#is_html' => $is_html,
     ];
@@ -238,15 +188,30 @@ class Mailer implements MailerInterface {
       // Request optimization so that the CssOptimizer performs essential
       // processing such as @include.
       $css_files = $this->assetResolver->getCssAssets($assets, TRUE);
-      $css = $this->cssCollectionRenderer->render($css_files);
+      $css = '';
+      foreach ($css_files as $file) {
+        $css .= file_get_contents($file['data']);
+      }
       $render['#css'] = $css;
     }
 
     $output = (string) $this->renderer->renderPlain($render);
+    $message->sending();
+
+    // Replace tokens.
+    if ($message->requiresTokenReplace()) {
+      $message->subject($this->token->replace(Html::escape($message->getSubject()), $message->getData(), $message->getTokenOptions()));
+      $output = $this->token->replace($output, $message->getData(), $message->getTokenOptions());
+    }
+
+    // Convert relative URLs to absolute.
+    $output = Html::transformRootRelativeUrlsToAbsolute($output, \Drupal::request()->getSchemeAndHttpHost());
+
     if ($is_html) {
       $message->html($output);
     }
     else {
+      // @todo Or maybe use league/html-to-markdown?
       $message->text((new Html2Text($output))->getText());
     }
   }
