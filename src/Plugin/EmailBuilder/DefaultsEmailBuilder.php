f6414dc1faee06ec96ddf679ce426beb89487a16
AdamPS <AdamPS@2650563.no-reply.drupal.org>
2021-10-28 15:49:54 +0100

    Fallback to default theme for email library

--- src/Plugin/EmailBuilder/DefaultsEmailBuilder.php
+++ src/Plugin/EmailBuilder/DefaultsEmailBuilder.php
@@ -2,8 +2,10 @@
 
 namespace Drupal\symfony_mailer\Plugin\EmailBuilder;
 
+use Drupal\Core\Asset\LibraryDiscoveryInterface;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Theme\ThemeManagerInterface;
 use Drupal\symfony_mailer\EmailBuilderBase;
 use Drupal\symfony_mailer\RenderedEmailInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -29,6 +31,20 @@ class DefaultsEmailBuilder extends EmailBuilderBase implements ContainerFactoryP
   protected $configFactory;
 
   /**
+   * The theme manager.
+   *
+   * @var \Drupal\Core\Theme\ThemeManagerInterface
+   */
+  protected $themeManager;
+
+  /**
+   * The library discovery service.
+   *
+   * @var \Drupal\Core\Asset\LibraryDiscoveryInterface
+   */
+  protected $libraryDiscovery;
+
+  /**
    * @param array $configuration
    *   A configuration array containing information about the plugin instance.
    * @param string $plugin_id
@@ -37,10 +53,16 @@ class DefaultsEmailBuilder extends EmailBuilderBase implements ContainerFactoryP
    *   The plugin implementation definition.
    * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
    *   The configuration factory.
+   * @param \Drupal\Core\Theme\ThemeManagerInterface $theme_manager
+   *   The theme manager.
+   * @param \Drupal\Core\Asset\LibraryDiscoveryInterface $library_discovery
+   *   The library discovery service.
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, ConfigFactoryInterface $config_factory) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, ConfigFactoryInterface $config_factory, ThemeManagerInterface $theme_manager, LibraryDiscoveryInterface $library_discovery) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->configFactory = $config_factory;
+    $this->themeManager = $theme_manager;
+    $this->libraryDiscovery = $library_discovery;
   }
 
   /**
@@ -51,7 +73,9 @@ class DefaultsEmailBuilder extends EmailBuilderBase implements ContainerFactoryP
       $configuration,
       $plugin_id,
       $plugin_definition,
-      $container->get('config.factory')
+      $container->get('config.factory'),
+      $container->get('theme.manager'),
+      $container->get('library.discovery')
     );
   }
 
@@ -65,9 +89,12 @@ class DefaultsEmailBuilder extends EmailBuilderBase implements ContainerFactoryP
     $email->getInner()->sender($from)
       ->getHeaders()->addTextHeader('X-Mailer', 'Drupal');
 
-    // @todo Fallback to default theme.
-    $mail_theme = \Drupal::theme()->getActiveTheme()->getName();
-    $email->addLibrary("$mail_theme/email");
+    // First try active theme then fallback to default theme.
+    $theme = $this->themeManager->getActiveTheme()->getName();
+    if (!$this->libraryDiscovery->getLibraryByName($theme, 'email')) {
+      $theme = $this->configFactory->get('system.theme')->get('default');
+    }
+    $email->addLibrary("$theme/email");
   }
 
 }
