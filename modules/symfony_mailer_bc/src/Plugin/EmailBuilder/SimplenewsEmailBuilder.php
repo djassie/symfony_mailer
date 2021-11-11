45496f391cdb7b8e274ebf20919fe9bc99148b7e
AdamPS <AdamPS@2650563.no-reply.drupal.org>
2021-11-11 17:24:04 +0000

    Use policy configuration for simplewnews module

--- modules/symfony_mailer_bc/src/Plugin/EmailBuilder/SimplenewsEmailBuilder.php
+++ modules/symfony_mailer_bc/src/Plugin/EmailBuilder/SimplenewsEmailBuilder.php
@@ -19,21 +19,7 @@ class SimplenewsEmailBuilder extends EmailBuilderBase {
    * {@inheritdoc}
    */
   public function build(UnrenderedEmailInterface $email) {
-    $key = $email->getSubType();
-    if ($key == 'subscribe_combined') {
-      $key = 'confirm_combined';
-    }
-
-    $config = \Drupal::config('simplenews.settings');
-    $body = [
-      '#type' => 'processed_text',
-      '#text' => $config->get("subscription.{$key}_body"),
-      '#format' => $config->get('subscription.text_format') ?: NULL,
-    ];
-
-    $email->setSubject($config->get("subscription.{$key}_subject"))
-      ->setBody($body)
-      ->addBuilder('token_replace', ['data' => $email->getParam('context'), 'pre_render' => TRUE]);
+    $email->addBuilder('token_replace', ['data' => $email->getParam('context')]);
   }
 
 }
