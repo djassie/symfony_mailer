23f9860334c97758fcf3e13ae07fc7b9c0399eaa
AdamPS <AdamPS@2650563.no-reply.drupal.org>
2021-11-12 13:07:28 +0000

    Use TWIG

--- src/Plugin/EmailBuilder/BodyEmailBuilder.php
+++ src/Plugin/EmailBuilder/BodyEmailBuilder.php
@@ -5,6 +5,7 @@ namespace Drupal\symfony_mailer\Plugin\EmailBuilder;
 use Drupal\Component\Render\PlainTextOutput;
 use Drupal\Component\Utility\Html;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Render\Markup;
 use Drupal\Core\Utility\Token;
 use Drupal\symfony_mailer\EmailBuilderBase;
 use Drupal\symfony_mailer\RenderedEmailInterface;
@@ -31,14 +32,20 @@ class BodyEmailBuilder extends EmailBuilderBase {
     // 1) Replace tokens. This must be before TWIG so we don't replace tokens in
     // any original body.
     if ($token = $email->getBuilder('token_replace')) {
-      $body = $token->replacePlain($body);
+      $body = $token->replaceMarkup($body);
     }
 
-    // 2) Replace TWIG variables. @todo Use TWIG?
+    // 2) Apply TWIG template.
     if ($variables = $email->getVariables()) {
-      $variables['body'] = $email->getHtmlBody();
-      $search = array_map(function($s) { return "{{ $s }}"; }, array_keys($variables));
-      $body = str_replace($search, array_values($variables), $body);
+      $variables['body'] = Markup::create($email->getHtmlBody());
+
+      $build = [
+        '#type' => 'inline_template',
+        '#template' => $body,
+        '#context' => $variables,
+      ];
+      $this->renderer = \Drupal::service('renderer'); // @todo
+      $body = $this->renderer->renderPlain($build);
     }
 
     // 3) Apply text format. This must be last so that it can act on any URLs
