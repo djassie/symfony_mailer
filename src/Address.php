fd2eb3440a807941edc4ddcc817a4fd02c1093e2
znerol <znerol@63999.no-reply.drupal.org>
2022-05-30 16:55:08 +0100

    Issue #3281725 by znerol: Missing namespace for LogicException in Address::create

--- src/Address.php
+++ src/Address.php
@@ -86,7 +86,7 @@ class Address implements AddressInterface {
       return new static($address->getAddress(), $address->getName());
     }
     else {
-      throw new LogicException('Cannot convert to address.');
+      throw new \LogicException('Cannot convert to address.');
     }
   }
 
