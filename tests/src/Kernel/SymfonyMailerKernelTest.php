e391a19f65d3f3d5a35817a915dc3fae803e92b2
Mingsong <2151-AMDS@users.noreply.drupalcode.org>
2023-09-16 13:49:42 +0100

    Issue #3371042 by Niklan, Mingsong, AdamPS: Drupal 10.1.0 new aggregation breaks InlineCssEmailAdjuster (tests only)

--- tests/src/Kernel/SymfonyMailerKernelTest.php
+++ tests/src/Kernel/SymfonyMailerKernelTest.php
@@ -60,4 +60,19 @@ class SymfonyMailerKernelTest extends KernelTestBase {
     $this->assertTo($to);
   }
 
+  /**
+   * Inline CSS adjuster test.
+   */
+  public function testInlineCss() {
+    $to = 'to@example.com';
+    // Test an email including the test library.
+    $this->emailFactory->newTypedEmail('symfony_mailer', 'test', $to)->addLibrary('symfony_mailer_test/inline_css_test')->send();
+    $this->readMail();
+    $this->assertNoError();
+    // The inline CSS from inline.text-small.css should appear.
+    $this->assertBodyContains('<h4 class="text-small" style="padding-top: 3px; padding-bottom: 3px; text-align: center; background-color: #0678be; color: white; font-size: smaller; font-weight: bold;">');
+    // The imported CSS from inline.day.css should appear.
+    $this->assertBodyContains('<span class="day" style="font-style: italic;">');
+  }
+
 }
